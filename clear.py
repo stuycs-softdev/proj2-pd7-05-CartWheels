19628ca844656e4c2ee84de398c8f224a2e3a4c3
Benjamin Attal <attal.benjamin@gmail.com>
2013-11-29 01:09:16 -0500

    Separate testing and clearing database

---
+++ clear.py
@@ -0,0 +1,12 @@
+from models import *
+
+carts = Cart()
+users = User()
+revs = Review()
+tags = Tag()
+
+renames = Rename()
+users.remove_all()
+revs.remove_all()
+tags.remove_all()
+renames.remove_all()
